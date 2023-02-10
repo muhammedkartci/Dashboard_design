aa3b824fbf96ad03cf1689461e18b24d56acc558
Muhammed Fatih Kartcı <muhammed.kartci@gmail.com>
2023-02-10 11:23:55 +0300

    sidebar tasarlanmaya başlandı

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <div class="sidebar-logo">
+                <img src="assets/images/logo.png" alt="logo">
+            </div>
+            <div class="sidebar-top">
+                <a class="nav-item">
+                    <div class="nav-icon">
+                        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.385 0H5.453C1.73 0 0 2.175 0 6.228v7.543C0 17.636 2.364 20 6.229 20h7.524c3.858 0 6.22-2.365 6.22-6.229l.027-4.8c0-4.048-1.69-6.03-5.47-6.03h-2.735c-.465 0-.904-.22-1.185-.594l-.882-1.174A2.928 2.928 0 007.385 0zM5.453 1.451h1.932c.466 0 .904.22 1.184.593l.883 1.176a2.938 2.938 0 002.342 1.173h2.736c2.925 0 4.02 1.285 4.02 4.574l-.026 4.8c0 3.066-1.713 4.782-4.77 4.782H6.228c-3.065 0-4.78-1.715-4.78-4.777V6.228c0-3.314 1.163-4.777 4.004-4.777zm9.89 11.093c0-.401-.325-.726-.725-.726H5.347l-.098.007a.726.726 0 00.098 1.445h9.27l.1-.007a.725.725 0 00.625-.72z" fill="#200E32" fill-rule="evenodd"/></svg>
+                    </div>
+                    <div class="nav-content">
+                        Nav 1
+                    </div>
+                </a>
+                <a class="nav-item">
+                    <div class="nav-icon">
+                        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.385 0H5.453C1.73 0 0 2.175 0 6.228v7.543C0 17.636 2.364 20 6.229 20h7.524c3.858 0 6.22-2.365 6.22-6.229l.027-4.8c0-4.048-1.69-6.03-5.47-6.03h-2.735c-.465 0-.904-.22-1.185-.594l-.882-1.174A2.928 2.928 0 007.385 0zM5.453 1.451h1.932c.466 0 .904.22 1.184.593l.883 1.176a2.938 2.938 0 002.342 1.173h2.736c2.925 0 4.02 1.285 4.02 4.574l-.026 4.8c0 3.066-1.713 4.782-4.77 4.782H6.228c-3.065 0-4.78-1.715-4.78-4.777V6.228c0-3.314 1.163-4.777 4.004-4.777zm9.89 11.093c0-.401-.325-.726-.725-.726H5.347l-.098.007a.726.726 0 00.098 1.445h9.27l.1-.007a.725.725 0 00.625-.72z" fill="#200E32" fill-rule="evenodd"/></svg>
+                    </div>
+                    <div class="nav-content">
+                        Nav 2
+                    </div>
+                </a>
+                <a class="nav-item">
+                    <div class="nav-icon">
+                        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.385 0H5.453C1.73 0 0 2.175 0 6.228v7.543C0 17.636 2.364 20 6.229 20h7.524c3.858 0 6.22-2.365 6.22-6.229l.027-4.8c0-4.048-1.69-6.03-5.47-6.03h-2.735c-.465 0-.904-.22-1.185-.594l-.882-1.174A2.928 2.928 0 007.385 0zM5.453 1.451h1.932c.466 0 .904.22 1.184.593l.883 1.176a2.938 2.938 0 002.342 1.173h2.736c2.925 0 4.02 1.285 4.02 4.574l-.026 4.8c0 3.066-1.713 4.782-4.77 4.782H6.228c-3.065 0-4.78-1.715-4.78-4.777V6.228c0-3.314 1.163-4.777 4.004-4.777zm9.89 11.093c0-.401-.325-.726-.725-.726H5.347l-.098.007a.726.726 0 00.098 1.445h9.27l.1-.007a.725.725 0 00.625-.72z" fill="#200E32" fill-rule="evenodd"/></svg>
+                    </div>
+                    <div class="nav-content">
+                        Nav 3
+                    </div>
+                </a>
+                <a class="nav-item">
+                    <div class="nav-icon">
+                        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.385 0H5.453C1.73 0 0 2.175 0 6.228v7.543C0 17.636 2.364 20 6.229 20h7.524c3.858 0 6.22-2.365 6.22-6.229l.027-4.8c0-4.048-1.69-6.03-5.47-6.03h-2.735c-.465 0-.904-.22-1.185-.594l-.882-1.174A2.928 2.928 0 007.385 0zM5.453 1.451h1.932c.466 0 .904.22 1.184.593l.883 1.176a2.938 2.938 0 002.342 1.173h2.736c2.925 0 4.02 1.285 4.02 4.574l-.026 4.8c0 3.066-1.713 4.782-4.77 4.782H6.228c-3.065 0-4.78-1.715-4.78-4.777V6.228c0-3.314 1.163-4.777 4.004-4.777zm9.89 11.093c0-.401-.325-.726-.725-.726H5.347l-.098.007a.726.726 0 00.098 1.445h9.27l.1-.007a.725.725 0 00.625-.72z" fill="#200E32" fill-rule="evenodd"/></svg>
+                    </div>
+                    <div class="nav-content">
+                        Nav 4
+                    </div>
+                </a>
+            </div>
+            <div class="sidebar-center"></div>
+            <div class="sidebar-bottom"></div>
+            <div class="sidebar-profile"></div>
+        </div>
+        <div class="main">
+            <div class="wrapper">
+                asdajskdkycydkuydkurkrkdrkdkr
+                asdajskdkycydkuydkurkrkdrkdkr
+            </div>
+        </div>
+    </div>
+</body>
+</html>
